7b8e6efccdcad0f4353deac117d1bb445268ec5a
ankitmankar0 <ankitmankar012@gmail.com>
2022-03-11 15:20:55 +0530

    Emp Attendence

--- EmployeeWages/EmployeeWages/Program.cs
+++ EmployeeWages/EmployeeWages/Program.cs
@@ -6,30 +6,21 @@ namespace EmployeeWages
     {
         static void Main(string[] args)
         {
-            //local variable
-            int employeePresent = 1;
-            int empHours = 0;
-            int empRatePerHour = 20;
-            int empWage = 0;
-            //creating object
+            int employeeState = 1;
             Random random = new Random();
-            //calling Next() for generating random values
             int Attendance = random.Next(0, 2);
             Console.WriteLine("Random number is :" + Attendance);
 
-            if (Attendance == employeePresent)
+            if (Attendance == employeeState)
             {
                 Console.WriteLine("Employee is Present \n");
-                empHours = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent \n");
-                empHours = 0;
 
             }
-            empWage = empRatePerHour * empHours;
-            Console.Read();
         }
     }
+
 }
